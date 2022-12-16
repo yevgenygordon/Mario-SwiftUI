b1d59d4b7d4ef523ca1c8d7b6bef3951b74fd380
yevgenygordon <ygordon1978@icloud.com>
2022-12-16 11:47:43 +0100

    Initial Commit

---
+++ Mario SwiftUI/Mario_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mario_SwiftUIApp.swift
+//  Mario SwiftUI
+//
+//  Created by Yevgeny Gordon on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Mario_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
